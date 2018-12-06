f929af8f8d3a0bc597d0ed1d0d97d406f0959293
bdodia <bdodia@users.noreply.github.com>
2018-12-06 23:54:56 +0000

    Added new component shell

---
+++ vuemaths/src/components/JavaQuestions.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>{{questions}}</h1>
+</template>
+
+!<script>
+export default {
+  name: "",
+  data() {
+    return {
+      questions: "Java Questions"
+    };
+  }
+};
+</script>
+
+<style  scoped>
+</style>
+
