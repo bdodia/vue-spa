9e66be6f6af1f7eec602f48ef4d3bec651f89b26
bdodia <bdodia@users.noreply.github.com>
2018-12-11 23:56:16 +0000

    Modified interface

--- vuemaths/src/components/JavaQuestions.vue
+++ vuemaths/src/components/JavaQuestions.vue
@@ -1,14 +1,38 @@
 <template>
-  <div>
-    <h1>{{title}}</h1>
-    <select>
-      <option
-        v-for="question in questions"
-        :key="question.id"
-        value="question.id"
-      >{{question.question}}</option>
-    </select>
-  </div>
+<div>
+  <body>
+    <section class="section">
+      <div class="container" @change="console.log($event)">
+        <h1 class="title">Java Quizzer</h1>
+        <p class="subtitle">Challenge yourself with Java questions!</p>
+        <div>
+          <h1>{{title}}</h1>
+          <el-select
+            v-model="valueSelection"
+            placeholder="Select"
+            @change="$emit('change', 'something')"
+          >
+            <el-option
+              v-for="question in questions"
+              :key="question.id"
+              :label="idCategory(question.id, question.category)"
+              :value="question.id"
+            ></el-option>
+          </el-select>
+        </div>
+
+        <div class="columns">
+          <div class="column is-narrow">
+            <div class="box" style="width: 200px;">
+              <p class="title is-5">Selected Question .....</p>
+              <p class="subtitle">Question text .....</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </body>
+</div>
 </template>
 
 <script>
@@ -16,12 +40,20 @@ export default {
   name: '',
   data () {
     return {
-      title: 'Java Questions',
-      questions: []
+      title: 'Pick a question',
+      questions: [],
+      valueSelection: ''
     }
   },
 
-  methods: {},
+  computed: {
+  },
+
+  methods: {
+    idCategory: function (id, category) {
+      return id + ' ' + category
+    }
+  },
 
   mounted () {
     fetch('http://localhost:3000/java')
